6cbcbc441a17fd1fa5248fb233dcfb3eb61d8697
mth75 <46650192+mth75@users.noreply.github.com>
2020-04-17 15:17:42 +0200

    Update demomode.sh

--- demomode.sh
+++ demomode.sh
@@ -4,6 +4,4 @@
 # Lets the user enable the demo mode
 #############################################
 
-sudo pkill -f BGM.py
-sudo pkill -f pngview
 sudo -u pi python3 /usr/share/pyshared/rungames.py
